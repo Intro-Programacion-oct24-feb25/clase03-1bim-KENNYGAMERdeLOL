887f25bd13fb2e63809040f2c4f7d4be6c0e94d5
jkparedes3@utpl,edu,ec <jkparedes3@utpl.edu.ec>
2024-10-23 11:53:12 -0500

    Kenny003

---
+++ ejercicios-IDE/Kenny003/src/kenny003/Kenny003.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kenny003;
+
+/**
+ *
+ * @author utpl
+ */
+public class Kenny003 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Kenny003");
+        
+    }
+    
+}
